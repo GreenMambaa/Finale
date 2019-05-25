594f3e8c404b503137973802b715690d81b8a3d7
Sidar Caliskan <chido91@gmail.com>
2019-05-25 22:32:52 +0200

    Adding navbar and README

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# les trois mousquetaires
+# Les Trois Mousquetaires
 
 This project was created by 3 beatiful minds. Three visionaries with their goals amongs the stars!!!
 
--- src/app/apiservice.service.ts
+++ src/app/apiservice.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiserviceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getBostadData() {
+    return this.http.get(`https://api.booli.se/listings?q=${stad}`);
+  }
+
 }
