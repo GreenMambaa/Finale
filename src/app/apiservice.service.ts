1064888f8dd85acbe10d586a9ce97b987d9cc6c3
Sidar Caliskan <chido91@gmail.com>
2019-05-29 13:49:41 +0200

    adding api

--- src/app/apiservice.service.ts
+++ src/app/apiservice.service.ts
@@ -8,8 +8,5 @@ export class ApiserviceService {
 
   constructor(private http: HttpClient) { }
 
-  getBostadData() {
-    return this.http.get(`https://api.booli.se/listings?q=nacka&limit=3&offset=0&callerId=[callerId]&time=1323793365&unique=3116053465361547264&hash=a053d19fcced8e180df1a40b3fc95b6560eee1af`);
-  }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiserviceService } from '../apiservice.service';
+import { HttpClient } from '@angular/common/http';
+import * as sha1 from 'sha1';
 
 @Component({
   selector: 'app-home',
@@ -9,14 +11,38 @@ import { ApiserviceService } from '../apiservice.service';
 })
 export class HomeComponent implements OnInit {
 
-  bostadData: object;
-  public angivenBostad = 'Stad';
+  // API call variabler
+  callerID = 'chido91';
+  timeStamp = new Date().getTime();
+  privateKey = 'ziAPZFmNF8or3kVQZEH38Q7AqqYP3fmYBKTGsoEg';
+  unique = (Math.random() * 1e19).toString(16);
+  sha: any;
+  stad = 'helsingborg';
 
-  constructor(private route: ActivatedRoute, private apiService: ApiserviceService) { }
+  bostadData: object;
+  angivenBostad: string;
+
+  getBostadData() {
+// tslint:disable-next-line: max-line-length
+    return this.http.get(`https://api.booli.se/listings?q=${this.stad}&limit=3&offset=0&callerId=${this.callerID}&time=${this.timeStamp}&unique=${this.unique}&hash=${this.sha}`);
+  }
+
+  constructor(private route: ActivatedRoute,
+              private apiService: ApiserviceService,
+              private http: HttpClient) {
+              this.sha = sha1(this.callerID + this.timeStamp + this.privateKey + this.unique);
+              console.log(this.sha);
+              }
 
   ngOnInit() {
 
+    this.getBostadData().subscribe(data => {
+      this.bostadData = data;
+      console.log(this.bostadData);
+    });
 
+    console.log(this.timeStamp);
+    console.log(this.unique);
 
   }
 
